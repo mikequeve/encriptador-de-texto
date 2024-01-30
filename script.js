af2120aba690d7e2d92c3ee53385fbedd6057f7e
Michael Vega <michaelvega46@gmail.com>
2024-01-30 14:32:51 -0600

    Agregando la funcion copiarTexto y haciendo cambios importantes al style.css

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ function encriptarTexto() {
       .replace(/u/gi, "ufat");
   d.querySelector("#texto-entrada").value = "";
   d.querySelector("#texto-salida").value = textoCodificado;
-  d.querySelector(".texto-salida").classList.toggle("active");
+  d.querySelector(".texto-salida").classList.remove("none");
+  d.querySelector(".campo-vacio").classList.add("none");
+  d.querySelector("#btn-copiar").classList.remove("none");
+}
+
+function copiarTexto() {
+  let $textoSalida = d.querySelector("#texto-salida");
+  $textoSalida.select();
+  $textoSalida.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText($textoSalida.value);
+  alert("Texto Copiado...");
+  d.querySelector("#texto-salida").value = "";
 }
