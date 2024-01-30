284038826d41138bf5969e1d121ad46cec0629c2
Michael Vega <michaelvega46@gmail.com>
2024-01-30 14:59:04 -0600

    Agregando la funcion desencriptarTexto al script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const d = document;
 
+function valores(texto) {
+  d.querySelector("#texto-entrada").value = "";
+  d.querySelector("#texto-salida").value = texto;
+  d.querySelector(".texto-salida").classList.remove("none");
+  d.querySelector(".campo-vacio").classList.add("none");
+  d.querySelector("#btn-copiar").classList.remove("none");
+}
+
 function encriptarTexto() {
   let $texto = d.querySelector("#texto-entrada").value.toLowerCase(),
     textoCodificado = $texto
@@ -8,11 +16,7 @@ function encriptarTexto() {
       .replace(/i/gi, "imes")
       .replace(/o/gi, "ober")
       .replace(/u/gi, "ufat");
-  d.querySelector("#texto-entrada").value = "";
-  d.querySelector("#texto-salida").value = textoCodificado;
-  d.querySelector(".texto-salida").classList.remove("none");
-  d.querySelector(".campo-vacio").classList.add("none");
-  d.querySelector("#btn-copiar").classList.remove("none");
+  valores(textoCodificado);
 }
 
 function copiarTexto() {
@@ -22,4 +26,15 @@ function copiarTexto() {
   navigator.clipboard.writeText($textoSalida.value);
   alert("Texto Copiado...");
   d.querySelector("#texto-salida").value = "";
+}
+
+function desencriptarTexto() {
+  let $textoCodificado = d.querySelector("#texto-entrada").value.toLowerCase(),
+    textoDesencriptado = $textoCodificado
+      .replace(/ai/gi, "a")
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+  valores(textoDesencriptado);
 }
